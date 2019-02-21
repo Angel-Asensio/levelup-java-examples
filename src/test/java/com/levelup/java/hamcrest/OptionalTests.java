84120cfa42f5295ab04f11b0c14655d12a72493a
Angel Asensio <angel.asensio-garcia@idealo.de>
2019-02-21 17:16:52 +0100

    small change to OptionalTests

--- src/test/java/com/levelup/java/hamcrest/OptionalTests.java
+++ src/test/java/com/levelup/java/hamcrest/OptionalTests.java
@@ -1,9 +1,6 @@
 package com.levelup.java.hamcrest;
 
-import static com.levelup.java.hamcrest.OptionalMatcher.emptyOptional;
-import static com.levelup.java.hamcrest.OptionalMatcher.optionalWithValue;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 import java.util.Optional;
@@ -16,13 +13,13 @@ public class OptionalTests {
     @Test
     public void assertThatOptionalIsEmpty() {
         final Optional<Object> empty = Optional.empty();
-        assertThat(empty, is(emptyOptional()));
+        assertThat(empty, CoreMatchers.is(OptionalMatcher.emptyOptional()));
     }
 
     @Test
     public void assertThatOptionalHasValue() {
         final Optional<Integer> optionalInteger = Optional.ofNullable(Integer.MAX_VALUE);
-        assertThat(optionalInteger, CoreMatchers.is(optionalWithValue(notNullValue())));
+        assertThat(optionalInteger, CoreMatchers.is(OptionalMatcher.optionalWithValue(notNullValue())));
     }
 
 }
